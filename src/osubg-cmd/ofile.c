5f7631ea5f528040a7a2163e0b6096fff46f97ff
unclenumbers <dhk.man2500@gmail.com>
2021-10-31 21:28:12 -0400

    unicode investigations

--- src/osubg-cmd/ofile.c
+++ src/osubg-cmd/ofile.c
@@ -5,16 +5,38 @@
 #include <windows.h>
 #include "osubg-cmd/ofile.h"
 
+#define _USE_MINGW_ANSI_STDIO 1
+
 int seekEventHeader( FILE *f, wchar_t *filename ) {
 	// [Events] = 8 chars
 	wchar_t
 		header[9] = { 0 },
 		current = 0;
 
-	FILE *test = _wfopen( L"test.txt", L"r" );
-	size_t fileChCount = 0;
-	while ( fgetwc( test )!= WEOF )
+	FILE
+		*test = _wfopen( L"test.txt", L"r" ),
+		*test2 = fopen( "test.txt", "r" );
+	size_t
+		fileChCount = 0,
+		fileChCount2 = 0;
+
+	wchar_t cha = 0;
+	char cha2 = 0;
+	while ( ( cha = fgetwc( test ) ) != WEOF ) {
 		fileChCount++;
+		wprintf( L"Character code wide: %hx\n", cha );
+	}
+
+	while ( ( cha2 = fgetc( test2 ) )!= EOF ) {
+		fileChCount2++;
+		wprintf( L"Character code: %hhx\n", cha2 );
+	}
+	
+	wchar_t *imstr = L"妹妹妹😊";
+	size_t sz = wcslen( imstr );
+	wprintf( L"size of string: %zu\n", sz );
+
+	wprintf( L"single-byte char count: %u\n", ( unsigned int )fileChCount2 );
 
 	wprintf( L"Character count: %llu\n", fileChCount );
 	wchar_t *fString = calloc( ( fileChCount + 1 ), sizeof( wchar_t ) );
@@ -27,11 +49,11 @@ int seekEventHeader( FILE *f, wchar_t *filename ) {
 	while ( ( ch = fgetwc( test ) ) != WEOF ) {
 		fString[ curCh++ ] = ch;
 		int chars = fwprintf( out, L"%s\n", fString );
-		NormalizeString( NormalizationC, fString, -1, NULL, 0 );
-		int normal = IsNLSDefinedString( COMPARE_STRING, 0, NULL, fString, -1 );
-		if ( !normal ) {
+		//NormalizeString( NormalizationC, fString, -1, NULL, 0 );
+		//int normal = IsNLSDefinedString( COMPARE_STRING, 0, NULL, fString, -1 );
+		//if ( !normal ) {
 			printf( "Input unicode bad! " );
-		}
+		//}
 
 		printf( "Chars written: %i\n", chars );
 	}
